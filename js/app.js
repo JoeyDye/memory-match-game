bdb8d16735a2e5dc556b0cde964cd3f665912a23
Joey Dye <joey.m.dye@gmail.com>
2018-01-09 06:09:53 -0600

    feat: Add test to determine correct match

--- js/app.js
+++ js/app.js
@@ -1,10 +1,29 @@
 
 const deck = document.querySelector('.deck');
+let clicked;
+let firstCard;
+let firstCardClass
+let secondCard;
+let secondCardClass
+
+function correctMatch () {
+  secondCard.classList.add('deck__card--show', 'deck__card--match');
+  firstCard.classList.replace('deck__card--open', 'deck__card--match');
+}
 
 
 function cardMatch (evt) {
-  if (evt.target.nodeName === 'LI') {
+  if (evt.target.nodeName === 'LI' && clicked === true) {
+    secondCard = evt.target;
+    secondCardClass = secondCard.children[0].classList[1];
+    if (firstCardClass === secondCardClass) {
+      correctMatch();
+    }
+  } else if (evt.target.nodeName === 'LI') {
     evt.target.classList.add('deck__card--show', 'deck__card--open');
+    firstCard = evt.target;
+    firstCardClass = firstCard.children[0].classList[1];
+    clicked = true;
   }
 }
 
