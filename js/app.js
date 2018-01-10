b510d9495f1c17252149bea23814829f73a91de3
Joey Dye <joey.m.dye@gmail.com>
2018-01-09 19:17:50 -0600

    feat: Add check to determine if player has won

--- js/app.js
+++ js/app.js
@@ -1,30 +1,50 @@
 
 const deck = document.querySelector('.deck');
+const cards = deck.children;
+let score = 0;
+let matchCount = 0;
 let clicked;
 let firstCard;
 let firstCardClass
 let secondCard;
 let secondCardClass
 
+// show selected card
+
 function showCard(card) {
   card.classList.add('deck__card--open');
 }
 
+// Check for match
+
 function correctMatch (card1, card2) {
   card1.classList.replace('deck__card--open', 'deck__card--match');
   card2.classList.add('deck__card--match');
+  clicked = false;
+  // Check for win
+  score += 1;
+  if (score > 7) { 
+    alert('You win!');
+  }
 }
+
+// Display incorrect match
 
 function incorrectMatch (card1, card2) {
   card1.classList.replace('deck__card--open', 'deck__card--fail');
   card2.classList.add('deck__card--fail');
   setTimeout(function reset () {
     card1.classList.remove('deck__card--fail');
-    card2.classList.remove('deck__card--fail');}, 2000)
+    card2.classList.remove('deck__card--fail');}, 1000)
+    clicked = false;
 }
 
+// Display correct match
 
 function cardMatch (evt) {
+  if (evt.target.classList.contains('deck__card--match')) {
+    return
+  }
   if (evt.target.nodeName === 'LI' && clicked === true) {
     secondCard = evt.target;
     secondCardClass = secondCard.children[0].classList[1];
@@ -33,11 +53,13 @@ function cardMatch (evt) {
     } else {
       incorrectMatch(firstCard, secondCard);
     }
-  } else if (evt.target.nodeName === 'LI') {
-    firstCard = evt.target;
-    firstCardClass = firstCard.children[0].classList[1];
-    showCard(firstCard);
-    clicked = true;
+  } else {
+    if (evt.target.nodeName === 'LI') {
+      firstCard = evt.target;
+      firstCardClass = firstCard.children[0].classList[1];
+      showCard(firstCard);
+      clicked = true;
+    }
   }
 }
 
